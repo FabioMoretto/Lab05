5f476e1310cd7abd0783e7e0ddf8a8738cbf8b12
fabio <fabio@LAPTOP-E702Q16I>
2019-07-11 18:17:45 +0200

    Progetto finale

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/controller/AnagrammiController.java
@@ -1,8 +1,61 @@
 package it.polito.tdp.anagrammi.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.anagrammi.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class AnagrammiController {
-
 	
-}
+	private Model modello;
 
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtInsert;
+
+    @FXML
+    private Button btnCalcolaAnagrammi;
+
+    @FXML
+    private TextArea txtCorretti;
+
+    @FXML
+    private TextArea txtErrati;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doCalcolaAnagrammi(ActionEvent event) {
+    	String parola = this.txtInsert.getText();
+    	this.txtCorretti.appendText(modello.getAnagrammiCorretti(parola));
+    	this.txtErrati.appendText(modello.getAnagrammiErrati(parola));
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	this.txtCorretti.clear();
+    	this.txtErrati.clear();
+    	this.txtInsert.clear();
+    }
+
+    @FXML
+    void initialize() {
+        assert txtInsert != null : "fx:id=\"txtInsert\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert btnCalcolaAnagrammi != null : "fx:id=\"btnCalcolaAnagrammi\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtCorretti != null : "fx:id=\"txtCorretti\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert txtErrati != null : "fx:id=\"txtErrati\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Anagrammi.fxml'.";
+
+    }
+}
